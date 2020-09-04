98f10054177f33d040858dce4770c97080364c86
Sofia Liendro <sofialiendro.s@gmail.com>
2020-09-04 14:38:33 -0300

    Agrego JS

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
-const buscarProductos = document.querySelector("#buscar-productos")
-const titulo = document.querySelector(".product-name")
+const busquedaProductos = document.querySelector("#buscar-productos")
+const tarjetas = document.getElementsByTagName("article")
+const filtroPuntaje = document.getElementsByClassName("rating")
 
-buscarProductos.oninput = () => {
-    console.log(titulo.textContent)
-    console.log(buscarProductos.value)
+
+busquedaProductos.oninput = () => {
+    
+    for (let tarjeta of tarjetas) {
+        
+        const titulo = tarjeta.dataset.nombre
+        const busqueda = busquedaProductos.value
+        
+        if (titulo.includes(busqueda)) {
+            tarjeta.classList.remove("no-mostrar")
+        }
+
+        else {
+            tarjeta.classList.add("no-mostrar")
+        }
+    }
+}
+
+
+for (let filtrosCheckbox of filtroPuntaje) {
+    filtrosCheckbox.onclick = () => {
+        
+        for (let tarjeta of tarjetas) {
+            if (filtrosCheckbox.checked) {
+                const puntajeFiltro = tarjeta.dataset.puntaje
+
+                if (filtrosCheckbox.value === puntajeFiltro) {
+                    tarjeta.classList.remove("no-mostrar")
+                }
+                else {
+                    tarjeta.classList.add("no-mostrar")
+                }
+            }
+            else {
+                tarjeta.classList.remove("no-mostrar")
+            }
+        }
+    }
 }
